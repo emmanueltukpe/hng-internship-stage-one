e4d9ad68a918f3317925d3c9804f77bb69387a2c
emmanueltukpe <tukpeemmanuel@gmail.com>
2022-10-31 14:10:02 +0100

    feat: first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const cors = require('cors')
+const express = require('express')
+var { StatusCodes } = require('http-status-codes')
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const data = { "slackUsername": "tukpe emmanuel", "backend": false, "age": 16, "bio": "I love to code" }
+
+app.get('/', (req, res) => {
+    res.status(StatusCodes.OK).json( data );
+});
+
+const port = process.env.PORT || 3000;
+
+const start = () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
