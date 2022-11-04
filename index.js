76c56dea269a7ed8aaa77d7965575987e3279a61
emmanueltukpe <tukpeemmanuel@gmail.com>
2022-11-04 16:56:32 +0100

    feat: added a post end point

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-require('dotenv').config();
-const cors = require('cors')
-const express = require('express')
-var { StatusCodes } = require('http-status-codes')
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+var { StatusCodes } = require("http-status-codes");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-const data = { "slackUsername": "emmanuel tukpe", "backend": false, "age": 25, "bio": "I love to code" }
+const data = {
+  slackUsername: "emmanuel tukpe",
+  backend: false,
+  age: 25,
+  bio: "I love to code",
+};
 
-app.get('/', (req, res) => {
-    res.status(StatusCodes.OK).json( data );
+app.get("/", (req, res) => {
+  res.status(StatusCodes.OK).json(data);
+});
+
+app.post("/", (req, res) => {
+  const { operation_type, x, y } = req.body;
+  var result;
+
+  switch (operation_type.value) {
+    case "addition":
+      result = x + y;
+      break;
+    case "subtraction":
+      result = x - y;
+      break;
+    case "multiplication":
+      result = x * y;
+      break;
+    default:
+      "Please enter a valid operation: addition, subtraction, multiplication";
+      result = "Invalid operation";
+      break;
+  }
+
+  let response = {
+    slackUsername: data.slackUsername,
+    operation_type: operation_type.value,
+    result,
+  };
+  res.status(StatusCodes.OK).json(response);
 });
 
 const port = process.env.PORT || 3000;
